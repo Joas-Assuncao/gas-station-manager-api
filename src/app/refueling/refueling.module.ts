b1ebec2ae060532df98d48df4ca05f4363bdf3c5
Joas-Assuncao <joassalvior@gmail.com>
2023-11-08 11:40:10 -0300

    feat: add driver e refueling modules

---
+++ src/app/refueling/refueling.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RefuelingEntity } from './refueling.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([RefuelingEntity])],
+})
+export class RefuelingModule {}
