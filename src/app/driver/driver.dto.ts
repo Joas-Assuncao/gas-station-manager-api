f7761e4593be2546b1b9b57dd0135588e21eebe3
Joas-Assuncao <joassalvior@gmail.com>
2023-11-08 19:26:31 -0300

    improve: add validation to refuelingDTO and remove refuelings from driverDTO

--- src/app/driver/driver.dto.ts
+++ src/app/driver/driver.dto.ts
@@ -1,11 +1,7 @@
-import { IsArray, IsNotEmpty, IsString } from 'class-validator';
-import { RefuelingDTO } from '../refueling/refueling.dto';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class DriverDTO {
   @IsString()
   @IsNotEmpty()
   name: string;
-
-  @IsArray()
-  refueling: RefuelingDTO[];
 }
--- src/app/refueling/refueling.dto.ts
+++ src/app/refueling/refueling.dto.ts
@@ -1,6 +1,17 @@
+import { IsNotEmpty, IsNumber, IsEnum } from 'class-validator';
+
+import { FuelType } from './enum/FuelType.enum';
+
 export class RefuelingDTO {
+  @IsNotEmpty()
+  @IsNumber()
   liters: number;
+
+  @IsNotEmpty()
+  @IsEnum(FuelType)
   fuelType: number;
-  totalPrice: number;
-  driver: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  fuelPrice: number;
 }
